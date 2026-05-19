ffa8053d6874b0bdde21c12b2d318c507ad8ac3a
MuskanJainTech <muskanjaintech@gmail.com>
2026-05-19 20:25:54 +0530

    Docker implementation

---
+++ src/main/java/dockerexample/springboot/docker/demo/controller/DockerController.java
@@ -0,0 +1,13 @@
+package dockerexample.springboot.docker.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @GetMapping("/docker")
+    public String dockerDemo(){
+        return "dockerizing spring boot application";
+    }
+}
